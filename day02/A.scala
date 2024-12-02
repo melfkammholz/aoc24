bdb384ccdd355e71c5e1cf12dbee9fb12b3bfb4f
Melf Kammholz <melf.kammholz@gmail.com>
2024-12-02 10:46:21 +0100

    add day02

---
+++ day02/A.scala
@@ -0,0 +1,22 @@
+import scala.io.Source
+
+@main
+def main(): Unit = {
+  var res = 0
+
+  for (line <- Source.stdin.getLines) {
+    val rep = line.split(" ").map(_.toInt)
+
+    val inRange: ((Int, Int)) => Boolean = t => t match {
+      case (a, b)  => (1 to 3).contains(b - a)
+    }
+
+    val zrep = rep.zip(rep.tail)
+    val okInc = zrep.forall(inRange)
+    val okDec = zrep.forall(t => inRange(t.swap))
+
+    res += { if (okInc || okDec) then 1 else 0 }
+  }
+
+  println(res)  // 269
+}
---
+++ day02/B.scala
@@ -0,0 +1,25 @@
+import scala.io.Source
+
+@main
+def main(): Unit = {
+  var res = 0
+
+  for (line <- Source.stdin.getLines) {
+    val rep = line.split(" ").map(_.toInt)
+
+    val inRange: ((Int, Int)) => Boolean = t => t match {
+      case (a, b)  => (1 to 3).contains(b - a)
+    }
+
+    val ok = (-1 until rep.length).exists { i =>
+      val drep = rep.take(i) ++ rep.drop(i + 1)
+      val zdrep = drep.zip(drep.tail)
+      val okInc = zdrep.forall(inRange)
+      val okDec = zdrep.forall(t => inRange(t.swap))
+      okInc || okDec
+    }
+    res += { if ok then 1 else 0 }
+  }
+
+  println(res)  // 337
+}
