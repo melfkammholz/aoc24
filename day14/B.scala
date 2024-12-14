e75a72f4a92d0390f2fd28ec2ded9a866018ce9f
Melf Kammholz <melf.kammholz@gmail.com>
2024-12-14 16:31:37 +0100

    refactor day14

--- day14/B.scala
+++ day14/B.scala
@@ -3,6 +3,9 @@ package day14
 import scala.math.{pow,sqrt}
 import scala.io.Source
 
+extension (x: Int)
+  infix def %%(m: Int) = (x % m + m) % m
+
 object B:
   def main(args: Array[String]): Unit =
     val (w, h): (Int, Int) = (101, 103)
@@ -11,29 +14,21 @@ object B:
       case s"p=$px,$py v=$vx,$vy" =>
         (px.toInt, py.toInt, vx.toInt, vy.toInt)
 
-    var u = Double.MaxValue
-    var res = 0
-    (0 until w * h).map
-    for t <- 0 until w * h do
+    val res = (0 until w * h).minBy: t =>
       var (cx, cy) = (0.0, 0.0)
       for (x, y, dx, dy) <- rs do
-        cx += ((x + t * dx) % w + w) % w
-        cy += ((y + t * dy) % h + h) % h
+        cx += (x + t * dx) %% w
+        cy += (y + t * dy) %% h
       cx /= rs.size
       cy /= rs.size
 
       var (vx, vy) = (0.0, 0.0)
       for (x, y, dx, dy) <- rs do
-        val tx = ((x + t * dx) % w + w) % w
-        val ty = ((y + t * dy) % h + h) % h
+        val (tx, ty) = ((x + t * dx) %% w, (y + t * dy) %% h)
         vx += (cx - tx) * (cx - tx)
         vy += (cy - ty) * (cy - ty)
-      vx /= rs.size
-      vy /= rs.size
 
-      if vx + vy < u then
-        u = vx + vy
-        res = t
+      vx + vy
 
     println(res)  // 8168
 
